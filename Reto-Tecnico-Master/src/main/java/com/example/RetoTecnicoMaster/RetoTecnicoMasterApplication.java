8f950629ea01972e9db662e6eaccb2ae49ede4dc
Valen <Valen@DESKTOP-7M9RAQD>
2022-06-12 18:41:28 -0500

    primer_commit

---
+++ Reto-Tecnico-Master/src/main/java/com/example/RetoTecnicoMaster/RetoTecnicoMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.RetoTecnicoMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoTecnicoMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoTecnicoMasterApplication.class, args);
+	}
+
+}
---
+++ Reto-Tecnico-Master/src/test/java/com/example/RetoTecnicoMaster/RetoTecnicoMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RetoTecnicoMaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoTecnicoMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
